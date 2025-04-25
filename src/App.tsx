a23fbf811b961b6ab334605f0d7b49688a7fbb16
Hak0bian <hakobyan7171@gmail.com>
2025-04-25 17:54:26 +0400

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Task } from './types';
+import axios from 'axios';
 import Form from './components/Form/Form';
 import TasksDiv from './components/TasksDiv/TasksDiv';
 import './App.css';
@@ -8,6 +9,15 @@ function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [taskCount, setTaskCount] = useState<number>(0);
 
+  useEffect(() => {
+    axios.get<Task[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then((res) => {
+        setTasks(res?.data);
+        setTaskCount(res?.data?.length);
+      })
+  }, [])
+
+  
   return (
     <section className='mainDiv'>
       <Form
